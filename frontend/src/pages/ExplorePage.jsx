149a99f5170cff5afd6580d992252e8a61b382b0
Jhoem Software <www.jhonlive90@gmail.com>
2024-03-24 23:26:14 -0500

    Explore view implementation

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-
 export const ExplorePage = () => {
     return (
-        <div>ExplorePage</div>
+        <div className="bg-glass max-w-2xl mx-auto p-5 border border-emerald-500 flex flex-col items-center gap-5">
+            <h1 className="text-xl">Explore Popular Repositories</h1>
+            <div className="flex gap-3">
+                <img src="./javascript.svg" alt="JS Logo" className="w-12 h-12 cursor-pointer" />
+                <img src="./typescript.svg" alt="TS Logo" className="w-12 h-12 cursor-pointer" />
+                <img src="./c++.svg" alt="C++ Logo" className="w-12 h-12 cursor-pointer" />
+                <img src="./python.svg" alt="PY Logo" className="w-12 h-12 cursor-pointer" />
+                <img src="./java.svg" alt="JAVA Logo" className="w-12 h-12 cursor-pointer" />
+            </div>
+        </div>
     )
 }
