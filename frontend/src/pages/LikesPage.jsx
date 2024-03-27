9a944fdf751ee99f856897c8fe474a70ae0f2811
Jhoem Software <www.jhonlive90@gmail.com>
2024-03-26 19:19:42 -0500

    Fix bug in LikesPage component

--- frontend/src/pages/LikesPage.jsx
+++ frontend/src/pages/LikesPage.jsx
@@ -2,36 +2,36 @@ import { FaHeart } from "react-icons/fa6";
 
 export const LikesPage = () => {
     return (
-        <div class="w-11/12 mx-auto my-5 relative overflow-x-auto rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right bg-glass">
-                <thead class="text-xs text-gray-400 uppercase bg-gray-800 border border-gray-800">
+        <div className="w-11/12 mx-auto my-5 relative overflow-x-auto rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right bg-glass">
+                <thead className="text-xs text-gray-400 uppercase bg-gray-800 border border-gray-800">
                     <tr>
-                        <th scope="col" class="px-16 py-3">
-                            <span class="sr-only">Image</span>
+                        <th scope="col" className="px-16 py-3">
+                            <span className="sr-only">Image</span>
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Username
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Date
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" className="px-6 py-3">
                             Action
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className="border-b border-gray-700">
-                        <td class="flex justify-center py-2">
-                            <img src="jhoemGitHub.jpg" class="w-14 h-14 border rounded-full" alt="Apple Watch"/>
+                        <td className="flex justify-center py-2">
+                            <img src="jhoemGitHub.jpg" className="w-14 h-14 border rounded-full" alt="Apple Watch"/>
                         </td>
-                        <td class="px-6 py-4 text-gray-900 dark:text-white">
+                        <td className="px-6 py-4 text-gray-900 dark:text-white">
                             Jhoem Software
                         </td>
-                        <td class="px-6 py-4 text-gray-900 dark:text-white">
+                        <td className="px-6 py-4 text-gray-900 dark:text-white">
                             2024, March 24
                         </td>
-                        <td class="px-6 py-4">
+                        <td className="px-6 py-4">
                             <FaHeart size={24} className="text-red-700 inline mx-2" /> Liked your profile
                         </td>
                     </tr>
