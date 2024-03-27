c6d414561528cc1b7e7866b5e887addc247f75b4
Jhoem Software <www.jhonlive90@gmail.com>
2024-03-27 14:09:18 -0500

    Fix bug CSS colors part 1

--- frontend/src/utils/utilities.js
+++ frontend/src/utils/utilities.js
@@ -11,8 +11,10 @@ export const validateLanguage = (lang) => {
 	const listImages = ['javascript','php','python','c','c++','csharp','css','html','go','java','swift','vue','typescript','shell', 'rust'];
 
     if(lang === null) lang = 'code'
-
-    if(lang === 'c#') lang = 'csharp'
+    
+    //console.log(lang)
+    
+    if(lang === 'C#') lang = 'csharp'
     
     lang = lang.toLowerCase();
 
