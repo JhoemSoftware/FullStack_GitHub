9037a2f1b9f985f23a1687a41f7c8347337c8d92
Jhoem Software <www.jhonlive90@gmail.com>
2024-04-12 13:47:04 -0500

    Fix bug background image

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext.jsx';
 import { App } from './App.jsx';
-import './index.css';
+import '../public/index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
