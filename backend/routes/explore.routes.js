c0d4a1fb7157dee2ee5ccd470d8ca0c67564e05f
Jhoem Software <www.jhonlive90@gmail.com>
2024-04-12 10:35:50 -0500

    Explore Page protected from backend app

--- backend/routes/explore.routes.js
+++ backend/routes/explore.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import getReposExplore from '../controllers/explore.controller.js';
+import { ensureAuth } from '../middlewares/ensureAuth.js';
 
 const router = express.Router();
 
-router.get('/repositories/:lang', getReposExplore);
+router.get('/repositories/:lang', ensureAuth, getReposExplore);
 
 export default router;
